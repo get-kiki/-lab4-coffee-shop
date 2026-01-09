4ab61e1e9c763a33f06b87725f661ac8e40b3f6a
unknown <Thanaphatte66@nu.ac.th>
2026-01-09 22:11:00 +0700

    Lab 7

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,12 @@
 const express = require('express')
-const cors = require('cors')
+let cors = require('cors')
 const { sequelize } = require('./models') // เรียกใช้ sequelize object ที่เราสร้างไว้
 const config = require('./config/config')
 
 const app = express()
 
 // --- Middleware Section ---
-app.use(express.json()) 
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
@@ -19,8 +19,7 @@ const port = config.port
 // สั่ง Sync ฐานข้อมูลก่อน แล้วค่อยเริ่ม Server
 // force: false หมายถึง ถ้ามีตารางอยู่แล้ว ไม่ต้องลบสร้างใหม่ (รักษาข้อมูลเดิมไว้)
 sequelize.sync({ force: false }).then(() => {
-    app.listen(config.port, function () {
-        console.log('CoffeeShop Server running on port ' + config.port)
-        })
+    app.listen(port, function () {
+        console.log('CoffeeShop Server running on port ' + port)
     })
-
+})
